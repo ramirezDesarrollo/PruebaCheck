e4e7c0a1dcc688e21001fa802055ea01ce51c112
ramirezDesarrollo <azael.hernandez@ramirezvargasabogados.com.mx>
2023-11-09 12:50:05 -0600

    nuevo cambio

--- client.js
+++ client.js
@@ -2,46 +2,67 @@ var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd
 var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
 
 TrelloPowerUp.initialize({
-    "card-buttons": function (t, options) {
-      return [
-        {
-          icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-          text: "SISTEMAS",
-          callback: function (t) {            
-            return t.popup({
-              title: "Estimation",
-              url: "estimate.html",
-            });
-          },
-        },
-      ];
+    'card-buttons': function(t, options) {
+        return [
+            {
+                icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
+                text: "SISTEMAS",
+                callback: function (t) {
+                    return asignarChecklistAutomatico(t);
+                },
+            },
+        ];
     },
-    //recuperamos nuestra estimacion usando t.get() 
+
     'card-badges': function(t, options) {
-      return t.get('card', 'shared', 'estimate')
-      .then(function(estimate) {
-        return [{
-          icon: estimate ? GREY_ROCKET_ICON : WHITE_ROCKET_ICON,
-          text: estimate || 'No Estimate!',
-          color: estimate ? null : 'red',
-        }];
-      });
+        return t.get('card', 'shared', 'estimate')
+        .then(function(estimate) {
+            return [{
+                icon: estimate ? GREY_ROCKET_ICON : WHITE_ROCKET_ICON,
+                text: estimate || 'No Estimate!',
+                color: estimate ? null : 'red',
+            }];
+        });
     },
-    //Tal como lo hicimos con la credencial de la tarjeta, podemos obtener la estimación actual usando el t.get()método de la biblioteca del cliente Power-Up 
+
     'card-detail-badges': function(t, options) {
-      return t.get('card', 'shared', 'estimate')
-      .then(function(estimate) {
-        return [{
-          title: 'Estimate',
-          text: estimate || 'No Estimate!',
-          color: estimate ? null : 'red',
-          callback: function(t) {
-            return t.popup({
-              title: "Estimation",
-              url: 'estimate.html',
-            });
-          }
-        }]
-      });
+        return t.get('card', 'shared', 'estimate')
+        .then(function(estimate) {
+            return [{
+                title: 'Estimate',
+                text: estimate || 'No Estimate!',
+                color: estimate ? null : 'red',
+                callback: function(t) {
+                    return t.popup({
+                        title: "Estimation",
+                        url: 'estimate.html',
+                    });
+                },
+            }];
+        });
     }
 });
+
+function asignarChecklistAutomatico(t) {
+    var defaultEmail = "azael.hernandez@ramirezvargasabogados.com";
+
+    return t.card.createChecklist({
+        title: 'Checklist por defecto',
+        idCard: t.getContext().card,
+    })
+    .then(function(checklist) {
+        return t.checklist.addChecklistItem({
+            idChecklist: checklist.id,
+            name: 'Asignar a ' + defaultEmail,
+            pos: 'top',
+        });
+    })
+    .then(function() {
+        return t.closePopup();
+    })
+    .then(function() {
+        return {
+            message: 'Checklist creado y asignado automáticamente',
+        };
+    });
+}
