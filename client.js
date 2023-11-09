69b725c8d87f883a208f9da567714e331daa1135
ramirezDesarrollo <azael.hernandez@ramirezvargasabogados.com.mx>
2023-11-09 15:01:40 -0600

    nuevo cambio

--- client.js
+++ client.js
@@ -1,5 +1,35 @@
 var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717';
 var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
+var DEFAULT_USER_EMAIL = "azael.hernandez@ramirezvargasabogados.com";
+
+
+function asignarChecklistAutomatico(t) {
+  var cardId = t.getContext().card;
+  // Crea un nuevo checklist
+  return t.api(`/cards/${cardId}/checklists`, 'post', {
+      name: 'Checklist por defecto',
+  })
+  .then(function(checklist) {
+      // Añade un elemento al checklist
+      return t.api(`/checklists/${checklist.id}/checkItems`, 'post', {
+          name: 'Asignar a ' + DEFAULT_USER_EMAIL,
+      });
+  })
+  .then(function() {
+      // Cierra el popup después de completar las acciones
+      return t.closePopup();
+  })
+  .then(function() {
+      return {
+          message: 'Checklist creado y asignado automáticamente',
+      };
+  })
+  .catch(function(error) {
+      console.error(error); // Registra cualquier error en la consola
+      // Manejar el error según sea necesario
+  });
+}
+
 
 TrelloPowerUp.initialize({
     "card-buttons": function (t, options) {
@@ -7,11 +37,12 @@ TrelloPowerUp.initialize({
         {
           icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
           text: "SISTEMAS",
-          callback: function (t) {            
-            return t.popup({
+          callback: function (t) {        
+            return asignarChecklistAutomatico(t);
+            /*return t.popup({
               title: "Estimation",
               url: "estimate.html",
-            });
+            });*/
           },
         },
       ];
