fe0a78372c8cc432a1f3f7bdc7198bf7b016b898
ramirezDesarrollo <azael.hernandez@ramirezvargasabogados.com.mx>
2023-11-09 09:49:19 -0600

    tercer cambio realizado dia 09 con card-badges

--- client.js
+++ client.js
@@ -1,3 +1,6 @@
+var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717';
+var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
+
 TrelloPowerUp.initialize({
     "card-buttons": function (t, options) {
       return [
@@ -13,16 +16,16 @@ TrelloPowerUp.initialize({
         },
       ];
     },
-
-    "card-badges": function(t, options) {
-    return t.get('card', 'shared', 'estimate')
-    .then(function(estimate) {
-      return [{
-        icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
-        text: estimate
-      }];
-    });
-  },
+    //recuperamos nuestra estimacion usando t.get() 
+    'card-badges': function(t, options) {
+      return t.get('card', 'shared', 'estimate')
+      .then(function(estimate) {
+        return [{
+          icon: estimate ? GREY_ROCKET_ICON : WHITE_ROCKET_ICON,
+          text: estimate || 'No Estimate!',
+          color: estimate ? null : 'red',
+        }];
+      });
+    },
   });
-
 
